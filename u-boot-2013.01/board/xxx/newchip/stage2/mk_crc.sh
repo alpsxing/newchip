db02f7c2c2c1dd7984f97c85593e4eb59852f42c
iunknown10 <iunknown@126.com>
2013-05-30 11:46:19 +0800

    modified header offset to 0x18

--- u-boot-2013.01/board/xxx/newchip/stage2/mk_crc.sh
+++ u-boot-2013.01/board/xxx/newchip/stage2/mk_crc.sh
@@ -1,2 +1,2 @@
 gcc -o mk_crc mk_crc.c board.c -Duart_printf=printf
-./mk_crc ./stage2_newchip_uart.bin --entry 0x08000800 -o stage2.img
+./mk_crc ./stage2_newchip_uart.bin --offset 0x18 --entry 0x08000800 -o stage2.img
--- u-boot-2013.01/board/xxx/newchip/stage2/mk_crc_mini.sh
+++ u-boot-2013.01/board/xxx/newchip/stage2/mk_crc_mini.sh
@@ -1,2 +1,2 @@
 gcc -o mk_crc mk_crc.c board.c -Duart_printf=printf
-./mk_crc ./stage2_newchip_mini.bin --entry 0x08002800 -o stage2_mini.img
+./mk_crc ./stage2_newchip_mini.bin --offset 0x18 --entry 0x08002800 -o stage2_mini.img
