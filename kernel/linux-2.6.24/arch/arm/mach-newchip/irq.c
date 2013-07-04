58e9a826a55960122e595cca7f0492c6e5b8bfa0
iunknown10 <iunknown@126.com>
2013-07-05 07:12:57 +0800

    add mach-newchip

---
+++ kernel/linux-2.6.24/arch/arm/mach-newchip/irq.c
@@ -0,0 +1,101 @@
+/*
+ * Interrupt handler for DaVinci boards.
+ *
+ * Copyright (C) 2006 Texas Instruments.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+
+#include <asm/hardware.h>
+#include <asm/io.h>
+#include <asm/mach/irq.h>
+
+
+#define which_irq_l(irq)         (0x01u << (irq)) 
+#define which_irq_h(irq)         (0x01u << ((irq) & 0x1f)) 
+
+#define write_irq_reg(addr, val)        __raw_writel(val, addr+(IO_ADDRESS(INTC_BASE))) 
+#define read_irq_reg(addr)              __raw_readl(addr+(IO_ADDRESS(INTC_BASE))) 
+#define set_irq_reg(addr, val)          write_irq_reg(addr, ((val) | read_irq_reg(addr)))
+#define clear_irq_reg(addr, val)        write_irq_reg(addr, (~(val) & read_irq_reg(addr)))
+
+
+static void newchip_irq_ack(u32 irq)
+{
+	//no irq ack
+}
+
+static void newchip_irq_mask(u32 irq)
+{
+    if (irq >= 32)
+    {
+        set_irq_reg(IRQ_REG_INTMASK_H, which_irq_h(irq));
+    }
+    else
+    {
+        set_irq_reg(IRQ_REG_INTMASK_L, which_irq_l(irq));
+    }
+
+}
+
+static void newchip_irq_unmask(u32 irq)
+{
+    if (irq >= 32)
+    {
+       clear_irq_reg(IRQ_REG_INTMASK_H, which_irq_h(irq));
+    }
+    else
+    {
+       clear_irq_reg(IRQ_REG_INTMASK_L, which_irq_l(irq));
+    }
+}
+
+static struct irq_chip newchip_irq_chip = {
+	.name		= "newchip_irq",
+	.ack		= newchip_irq_ack,
+	.mask		= newchip_irq_mask,
+	.unmask		= newchip_irq_unmask,
+};
+
+
+/*
+ * Initialize the AIC interrupt controller.
+ */
+void __init newchip_irq_init(void)
+{
+	u32 i;
+	
+	write_irq_reg(IRQ_REG_INTEN_L, 0xffffffff);//enable irq interrupt
+	write_irq_reg(IRQ_REG_INTEN_H, 0xffffffff);
+	write_irq_reg(IRQ_REG_INTMASK_L, 0xffffffff); //mask all irq interrupt
+	write_irq_reg(IRQ_REG_INTMASK_H, 0xffffffff);
+	write_irq_reg(IRQ_REG_INTFORCE_L, 0);
+	write_irq_reg(IRQ_REG_INTFORCE_H, 0);
+	write_irq_reg(FIQ_REG_INTEN, 0x03); //enable fiq interrupt
+	write_irq_reg(FIQ_REG_INTMASK, 0x03); //mask fiq interrupt
+	write_irq_reg(IRQ_REG_PLEVEL, 0);
+
+	for (i = 0; i < NR_NEWCHIP_IRQS; i++) {
+		set_irq_chip(i, &newchip_irq_chip);
+		set_irq_handler(i, handle_level_irq);
+		set_irq_flags(i, IRQF_VALID);//no probe and auto enable		
+	}
+}
+
---
+++ kernel/linux-2.6.24/include/asm-arm/arch-newchip/debug_uart.h
@@ -0,0 +1,90 @@
+
+#ifndef UART_DEBUG_H
+#define UART_DEBUG_H
+
+#include <asm/arch/hardware.h>
+#include <asm/arch/io.h>
+
+struct UR0431A_UART {
+	unsigned char rbr;		/* 0 */
+	int pad1:24;
+	unsigned char ier;		/* 1 */
+	int pad2:24;
+	unsigned char fcr;		/* 2 */
+	int pad3:24;
+	unsigned char lcr;		/* 3 */
+	int pad4:24;
+	unsigned char mcr;		/* 4 */
+	int pad5:24;
+	unsigned char lsr;		/* 5 */
+	int pad6:24;
+	unsigned char msr;		/* 6 */
+	int pad7:24;
+	unsigned char scr;		/* 7 */
+	int pad8:24;
+} __attribute__ ((packed));
+
+#define thr rbr
+#define iir fcr
+#define dll rbr
+#define dlh ier
+
+typedef volatile struct UR0431A_UART *UR0431A_UART_t;
+
+#define FCR_FIFO_EN     0x01		/* Fifo enable */
+#define FCR_RXSR        0x02		/* Receiver soft reset */
+#define FCR_TXSR        0x04		/* Transmitter soft reset */
+#define MCR_DTR         0x01
+#define MCR_RTS         0x02
+#define MCR_DMA_EN      0x04
+#define MCR_TX_DFR      0x08
+#define LCR_WLS_MSK	0x03		/* character length slect mask */
+#define LCR_WLS_5	0x00		/* 5 bit character length */
+#define LCR_WLS_6	0x01		/* 6 bit character length */
+#define LCR_WLS_7	0x02		/* 7 bit character length */
+#define LCR_WLS_8	0x03		/* 8 bit character length */
+#define LCR_STB		0x04		/* Number of stop Bits, off = 1, on = 1.5 or 2) */
+#define LCR_PEN		0x08		/* Parity eneble */
+#define LCR_EPS		0x10		/* Even Parity Select */
+#define LCR_STKP	0x20		/* Stick Parity */
+#define LCR_SBRK	0x40		/* Set Break */
+#define LCR_BKSE	0x80		/* Bank select enable */
+#define LSR_DR		0x01		/* Data ready */
+#define LSR_OE		0x02		/* Overrun */
+#define LSR_PE		0x04		/* Parity error */
+#define LSR_FE		0x08		/* Framing error */
+#define LSR_BI		0x10		/* Break */
+#define LSR_THRE	0x20		/* Xmit holding register empty */
+#define LSR_TEMT	0x40		/* Xmitter empty */
+#define LSR_ERR		0x80		/* Error */
+#define LCR_8N1		0x03
+
+
+
+#define com_port ((UR0431A_UART_t)IO_ADDRESS(UART0_BASE))	
+
+static void uart_putc(char c)
+{
+
+	while ((com_port->lsr & LSR_THRE) == 0);
+	if (c == '\n')
+	{
+	    com_port->thr = '\r';	
+		while ((com_port->lsr & LSR_THRE) == 0);
+		com_port->thr = '\n';
+	}
+	else	
+		com_port->thr = c;
+
+}
+
+static void uart_puts (const char *s)
+{
+	while (*s) {
+		uart_putc (*s++);
+	}
+}
+
+
+
+#endif
